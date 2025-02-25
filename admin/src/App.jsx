55083c733adebdeadf475337d94b9a872cb821bf
viveklams <viveklamgadey@gmail.com>
2025-02-25 17:15:52 +0530

    working on the side bar compoenet with lucide-react as icons and framermotion for animation of the project

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <div className="text-red-500 text-4xl bg-blue-500">hello</div>;
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import {
+  BarChart2,
+  DollarSign,
+  Settings,
+  ShoppingBag,
+  ShoppingCart,
+  TrendingUp,
+  Users,
+} from "lucide-react";
+
+const SIDEBAR_ITEMS = [
+  {
+    name: "Overview",
+    icon: BarChart2,
+    color: "#6366f1",
+    href: "/",
+  },
+  { name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/products" },
+  { name: "Users", icon: Users, color: "#EC4899", href: "/users" },
+  { name: "Sales", icon: DollarSign, color: "#10B981", href: "/sales" },
+  { name: "Orders", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
+  { name: "Analytics", icon: TrendingUp, color: "#3B82F6", href: "/analytics" },
+  { name: "Settings", icon: Settings, color: "#6EE7B7", href: "/settings" },
+];
+
+const Sidebar = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+    
+
+  return <div></div>;
+};
+
+export default Sidebar;
