26e84b667ef9c59f54d7a6c9377f915db3cd8947
viveklams <viveklamgadey@gmail.com>
2025-02-27 01:20:17 +0530

    overviewpage competed with lots of styling and framermotion and react CHarts:

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,14 +1,17 @@
 import {
   BarChart2,
-  DollarSign,
   Settings,
   ShoppingBag,
   ShoppingCart,
   TrendingUp,
   Users,
+  DollarSign,
+  Menu,
 } from "lucide-react";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SIDEBAR_ITEMS = [
   {
@@ -17,7 +20,12 @@ const SIDEBAR_ITEMS = [
     color: "#6366f1",
     href: "/",
   },
-  { name: "Products", icon: ShoppingBag, color: "#8B5CF6", href: "/products" },
+  {
+    name: "Products",
+    icon: ShoppingBag,
+    color: "#8B5CF6",
+    href: "/products",
+  },
   { name: "Users", icon: Users, color: "#EC4899", href: "/users" },
   { name: "Sales", icon: DollarSign, color: "#10B981", href: "/sales" },
   { name: "Orders", icon: ShoppingCart, color: "#F59E0B", href: "/orders" },
@@ -34,7 +42,45 @@ const Sidebar = () => {
         isSidebarOpen ? "w-64" : "w-20"
       }`}
       animate={{ width: isSidebarOpen ? 256 : 80 }}
-    ></motion.div>
+    >
+      <div className="h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700 ">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit"
+        >
+          <Menu size={24} />
+        </motion.button>
+
+        <nav className="mt-8 flex-grow">
+          {SIDEBAR_ITEMS.map((item) => {
+            const IconComponent = item.icon; // Destructure the icon as a component
+            return (
+              <Link key={item.href} to={item.href}>
+                <motion.div className="flex items-center p-4 text-sm font-medium rounded-lg hover:bg-gray-700 transition-colors mb-2">
+                  <IconComponent size={20} style={{ color: item.color }} />
+                  {/* {" "} */}
+                  <AnimatePresence>
+                    {isSidebarOpen && (
+                      <motion.span
+                        className="ml-4 whitespace-nowrap"
+                        initial={{ opacity: 0, width: 0 }}
+                        animate={{ opacity: 1, width: "auto" }}
+                        exit={{ opacity: 0, width: 0 }}
+                        transition={{ duration: 0.2, delay: 0.3 }}
+                      >
+                        {item.name}
+                      </motion.span>
+                    )}
+                  </AnimatePresence>
+                </motion.div>
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </motion.div>
   );
 };
 
--- admin/src/pages/OverviewPage.jsx
+++ admin/src/pages/OverviewPage.jsx
@@ -1,5 +1,60 @@
+import Header from "../components/common/Header";
+import { motion } from "framer-motion";
+import StateCard from "../components/common/StateCard";
+import { Zap, Users, ShoppingBag, BarChart2 } from "lucide-react";
+import SalesOverViewChart from "../components/overview/SalesOverViewChart";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../components/overview/SalesChannelChart";
+
 const OverviewPage = () => {
-  return <div>OverviewPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+
+      <main className="max-w-7xl max-auto py-6 px-4 lg:px-8 xl:px-20">
+        {/* STATS */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StateCard
+            name="Total Sales"
+            icon={Zap}
+            value="$12,345"
+            color="#6366F1"
+          />
+          <StateCard
+            name="New Users"
+            icon={Users}
+            value="1,234"
+            color="#8B5CF6"
+          />
+          <StateCard
+            name="Total Products"
+            icon={ShoppingBag}
+            value="567"
+            color="#EC4899"
+          />
+          <StateCard
+            name="Conversion Rate"
+            icon={BarChart2}
+            value="12.5%"
+            color="#10B981"
+          />
+        </motion.div>
+
+        {/*CHARTS*/}
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <SalesOverViewChart />
+          <CategoryDistributionChart />
+          <SalesChannelChart />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default OverviewPage;
